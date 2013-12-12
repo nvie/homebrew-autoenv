115028aff12939089dc9214d3df7ee69aaba9cae
Vincent Driessen <vincent@3rdcloud.com>
2013-12-12 23:32:38 +0100

    Add version and conflict info.

--- nvie-autoenv.rb
+++ nvie-autoenv.rb
@@ -4,6 +4,8 @@ class Autoenv < Formula
   homepage 'https://github.com/nvie/autoenv'
   url 'https://github.com/nvie/autoenv/archive/v0.1.0-nvie.tar.gz'
   sha1 '9b5a27f9f96424c7adec5867a391839862ba7407'
+  version 0.1.0-nvie
+  conflicts_with 'autoenv', :because => 'because this is nvie\'s fork'
 
   head 'https://github.com/nvie/autoenv.git', :branch => 'master'
 
