16819e850e45d9a9ee061d9aea8f8c671eecf522
Vincent Driessen <vincent@3rdcloud.com>
2013-12-12 23:11:12 +0100

    Fix incorrect SHA

--- autoenv.rb
+++ autoenv.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Autoenv < Formula
   homepage 'https://github.com/nvie/autoenv'
   url 'https://github.com/nvie/autoenv/archive/v0.1.0-nvie.tar.gz'
-  sha1 '48380af4d8a4cbb6627e136bc3aeeedc'
+  sha1 '9b5a27f9f96424c7adec5867a391839862ba7407'
 
   head 'https://github.com/nvie/autoenv.git', :branch => 'master'
 
